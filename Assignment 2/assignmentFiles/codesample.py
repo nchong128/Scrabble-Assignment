8330995fddd60c6e41e03ed73b5faa351c407374
Nicholas Chong <ncho0010@student.monash.edu>
2018-04-23 20:53:10 +1000

    Minor adjustments to tilePlacer function in codesample

--- Assignment 2/assignmentFiles/codesample.py
+++ Assignment 2/assignmentFiles/codesample.py
@@ -1,12 +1,19 @@
 # REPRESENTS A 5X5 BOARD- DO NOT DELETE
 Board = [['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', ''], ['', '', '', '', '']]
 boardSize = len(Board)
-
+firstMove = False
+wordLocation = ["1","1","H"]
 # REPRESENTS A 5X5 BOARD- DO NOT DELETE
+
 def tilePlacer(chosenWord,location):
+    row = int(location[0])
+    column = int(location[1])
+    
     if firstMove == True:
         pass
     elif location[2] == "H":
-        for i in range(len(chosenWord)):
-            Board[location[0]][location[1]]
+        for i in range(column, len(chosenWord)):
+            Board[row][i] = chosenWord[i]
 
+tilePlacer("TONE",wordLocation)
+print(Board)
