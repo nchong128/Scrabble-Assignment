3f99bdacf222061a2e66f6850a0bafbf261d82e0
Nicholas Chong <ncho0010@student.monash.edu>
2018-04-25 22:47:45 +1000

    Implemented everything back to the main file

--- Assignment 2/assignmentFiles/scrabble2.py
+++ Assignment 2/assignmentFiles/scrabble2.py
@@ -208,7 +208,50 @@ def locationSyntaxCheck(location):
         return False
     return True
 
+#This function will check if the word will be allowed to be placed into the Board, given a
+#syntactically correct location and valid word.
+def locationPlaceCheck(chosenWord,location):
+    middleOfBoard = len(Board) // 2
+    
+    row = int(location[0])
+    column = int(location[1])
 
+    horizontalLimit = column + len(chosenWord)
+    verticalLimit = row + len(chosenWord)
+    
+    #Checks if word is placed in the middle of the board by ensuring the row and columns
+    #are equal to the middle of the board. Applicable only for the first move.
+    if firstMove == True and (row != middleOfBoard or column != middleOfBoard):
+        print("First move must have the word placed in the middle of the board.")
+        return False
+    
+    #Checks if word is can be placed within the Board's dimension limits
+    elif (location[2] == "H" and horizontalLimit > len(Board)) or (location[2] == "V" and verticalLimit > len(Board)):
+        print("Word cannot fit on the Board.")
+        return False
+
+    return True
+
+#This function will place the VALID word into Board using the location (r:c:d) given
+#META: Will need adjustment to changign everything after the first tile
+def tilePlacer(chosenWord,location):
+    row = int(location[0])
+    column = int(location[1])
+    WordIndex = 0
+
+    #Dealing with where the word is to be placed horizontally
+    if location[2] == "H":
+        endPoint = column + len(chosenWord)
+        for i in range(column, endPoint):
+            Board[row][i] = chosenWord[WordIndex]
+            WordIndex += 1
+
+    #Dealing with where the word is to be placed vertically
+    elif location[2] == "V": #META: May need changing to just else: ?
+        endPoint = row + len(chosenWord)
+        for i in range(row, endPoint):
+            Board[i][column] = chosenWord[WordIndex]
+            WordIndex += 1
 
 while True:
     #Input for chosenWord and location, followed by adjusting the format of them
@@ -222,6 +265,7 @@ while True:
         print("Better luck next time!")
         break
     
-    elif letterCheck(chosenWord) and dictionaryCheck(chosenWord):
-        if tileCheck(chosenWord):
-            pass
+    elif letterCheck(chosenWord) and dictionaryCheck(chosenWord) and tileCheck(chosenWord):
+        if locationPlaceCheck(chosenWord,wordLocation) and locationSyntaxCheck(chosenWord,wordLocation):
+            tilePlacer(chosenWord,wordLocation)
+            print(Board)
