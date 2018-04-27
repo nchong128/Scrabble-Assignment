fb8c37941fccb74c1751c39999c9d248371d2011
Nicholas Chong <ncho0010@student.monash.edu>
2018-04-27 21:08:47 +1000

    Began creation of tileRemover

--- Assignment 2/assignmentFiles/codesample.py
+++ Assignment 2/assignmentFiles/codesample.py
@@ -4,4 +4,7 @@ boardSize = len(Board)
 wordLocation = ["0","6","V"]
 chosenWord = "SENT"
 firstMove = False
+myTiles = ["B","S","N","O","E","U","T"]
 # REPRESENTS A 5X5 BOARD- DO NOT DELETE
+
+def tileRemover(chosenWord):
